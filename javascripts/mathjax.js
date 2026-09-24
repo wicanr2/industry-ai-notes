11a0f1310259cf00383c7f67f3c10703e0dd2ac5
Chun-Yu Wang <>
2026-09-24 09:07:53 +0800

    本地部署網站：3444b22

---
+++ javascripts/mathjax.js
@@ -0,0 +1,18 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"], ["$", "$"]],
+    displayMath: [["\\[", "\\]"], ["$$", "$$"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: "\\btex2jax_ignore\\b",
+    processHtmlClass: "\\btex2jax_process\\b"
+  }
+};
+
+document$.subscribe(() => {
+  if (window.MathJax?.typesetPromise) {
+    MathJax.typesetPromise();
+  }
+});
